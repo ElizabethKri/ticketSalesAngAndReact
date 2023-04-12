964d3e62d60f744c815b5127418b03f1a8144e31
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-12 20:31:04 +0300

    add reactive form (Form group)

--- src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
+++ src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ITour} from "../../../models/tours";
 import {ActivatedRoute} from "@angular/router";
 import {TiсketsStorageService} from "../../../services/tiсkets-storage/tiсkets-storage.service";
+import {IUser} from "../../../models/users";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../services/user/user.service";
 
 
 
@@ -12,11 +15,26 @@ import {TiсketsStorageService} from "../../../services/tiсkets-storage/tiсket
 })
 export class TicketItemComponent implements OnInit {
   ticket: ITour | undefined;
+  user: IUser;
+  userForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private ticketStorage: TiсketsStorageService) { }
+              private ticketStorage: TiсketsStorageService,
+              private userService: UserService) { }
 
   ngOnInit(): void {
+    //записываем пользователя
+    this.user = this.userService.getUser()
+    //создание формГруппы
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', {validators: Validators.required}),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      cardNumber: new FormControl(this.user?.cardNumber),
+      birthDay: new FormControl(''),
+      age: new FormControl(),
+      citizen: new FormControl('')
+    })
+
     //считываем параметры
     const routeIdParam = this.route.snapshot.paramMap.get('id'); // значение, которое передано при маршрутизации (связано с тек. роутингом)
     const queryIDParam = this.route.snapshot.queryParamMap.get('id'); // глобальный Map всех параметров
@@ -28,6 +46,23 @@ export class TicketItemComponent implements OnInit {
       this.ticket = ticketStorage.find((el) => el.id === paramValueId);
       console.log('this.ticket', this.ticket)
     }
+  }
+
+  ngAfterViewInit(): void{
+    //setCardNumber
+    //обращаемся на ссылку группу форм, значение карты
+    this.userForm.controls["cardNumber"].setValue(this.user?.cardNumber);
+    // this.userForm.patchValue ({
+    // cardNumber: this.user.cardNumber
+    //});
+  }
+
+  onSubmit(): void{
+
+  }
+
+  selectDate(ev: Event): void{
+
   }
 
 }
