d916a7b210403ba9217427fdc2d5dec78e9fa89e
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-22 12:11:00 +0300

    add change password

--- src/app/pages/setting/setting.module.ts
+++ src/app/pages/setting/setting.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
 import {SettingComponent} from "./setting.component";
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import {TabViewModule} from "primeng/tabview";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
 
 
 @NgModule({
-  declarations: [SettingComponent],
+  declarations: [SettingComponent, ChangePasswordComponent],
   imports: [
     CommonModule,
-    SettingRoutingModule
+    SettingRoutingModule,
+    TabViewModule,
+    FormsModule,
+    InputTextModule
   ]
 })
 export class SettingModule { }
